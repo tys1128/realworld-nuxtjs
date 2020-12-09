3dd5bbc18b60fe5d86eb87e32b74609e1180a72b
tys1128 <tys1128@outlook.com>
2020-12-09 23:24:05 +0800

    其余页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,19 +7,43 @@ module.exports = {
         {
           path: '/',
           component: resolve(__dirname, 'pages/layout/'),
-          children: [{
-            path: '',
-            name: 'home',
-            component: resolve(__dirname, 'pages/home/')
-          },{
-            path: '/login',
-            name: 'login',
-            component: resolve(__dirname, 'pages/login/')
-          },{
-            path: '/register',
-            name: 'register',
-            component: resolve(__dirname, 'pages/login/')
-          }]
+          children: [
+            {
+              path: '',
+              name: 'home',
+              component: resolve(__dirname, 'pages/home/')
+            },
+            {
+              path: '/login',
+              name: 'login',
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/register',
+              name: 'register',
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/profile/:username',
+              name: 'profile',
+              component: resolve(__dirname, 'pages/profile/')
+            },
+            {
+              path: '/setting',
+              name: 'setting',
+              component: resolve(__dirname, 'pages/setting/')
+            },
+            {
+              path: '/editor',
+              name: 'editor',
+              component: resolve(__dirname, 'pages/editor/')
+            },
+            {
+              path: '/article/:slug',
+              name: 'article',
+              component: resolve(__dirname, 'pages/article/')
+            },
+          ]
         }
       ])
     }
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -88,7 +88,7 @@
 
 <script>
 export default {
-  name: 'ProfileIndex'
+  name: 'UserProfile'
 }
 </script>
 
